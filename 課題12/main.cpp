e1e6d2746527a87c998f1c4796b281dc5cdc58ae
ootu_sakuto <230415@jc-21.jp>
2024-06-26 11:42:59 +0900

    1

--- 課題12/main.cpp
+++ 課題12/main.cpp
@@ -1,27 +1,6 @@
-<<<<<<< HEAD:課題6/main.cpp
-#include "Calclation.h"
 
 int main()
 {
 	float a = 42.195f, b = 3.14f;
 
-	calclation x;
-	x.SetA(a);
-	x.SetB(b);
-	x.Disp();
-}
-=======
-#include "Sample.h"
-
-
-int main(){
-	SampleClass *a;
-		a = new SampleClass();
-
-		a->Input();
-		a->Plus();
-		a->Disp();
-
-		delete a;
 	}
->>>>>>> 8a9f4d693c451720ed6b98688a11ed2f90c9b967:課題12/main.cpp
