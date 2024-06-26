08a30c5c40176cd16f9e4551f050b302cc983469
ootu_sakuto <230415@jc-21.jp>
2024-06-26 16:19:19 +0900

    2

--- C++zissyuu/main.cpp
+++ C++zissyuu/main.cpp
@@ -1,13 +1,16 @@
 #include "Sample.h"
 
+SampleClass* a;
+
 
 int main()
 {
 	//SampleClassクラスのインスタンス（実態）を作る 
-	SampleClass a;
-
+	a = new SampleClass;
 	//３つのメンバ関数を呼び出す 
-	a.Input();
-	a.Plus();
-	a.Disp();
+	a->Input();
+	a->Plus();
+	a->Disp();
+
+	delete a;
 }
